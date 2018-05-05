1f1e00e1f657e485bf9bf82b9d4459d3c8a6ae2a
Shimi Bar <shimi.b@zooz.com>
2018-05-05 19:00:03 +0300

    initial module two

---
+++ ModuleTwo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class ModuleTwo extends React.Component {
+  render () {
+    return (
+      <div>
+        <div className='header' style={{textAlign: 'center', height: '100vh'}}>
+          module two page
+        </div>
+      </div>
+    )
+  }
+}
